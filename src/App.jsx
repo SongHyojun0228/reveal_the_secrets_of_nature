eb015c896f0d55d1c0824be6823c5da87a8691af
SongHyojun0228 <thdgywns2300@gmail.com>
2026-02-09 14:07:42 +0900

    add Vercel Web Analytics code

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Terms from './pages/Terms';
 import Contact from './pages/Contact';
 import Blog from './pages/Blog';
 import BlogPost from './pages/BlogPost';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   const { theme } = useTheme();
